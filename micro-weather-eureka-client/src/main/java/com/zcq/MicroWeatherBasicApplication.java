7958a8bd941bed76bf90aa12457c57fdc29d262e
zcq <zcq960921@163.com>
2019-04-17 22:44:51 +0800

    集成Eureka客户端

---
+++ micro-weather-eureka-client/src/main/java/com/zcq/MicroWeatherBasicApplication.java
@@ -0,0 +1,17 @@
+package com.zcq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class MicroWeatherBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroWeatherBasicApplication.class, args);
+    }
+
+
+}
